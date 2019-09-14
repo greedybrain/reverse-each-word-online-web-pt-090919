26804a9f8a7588ea6907ea2865dab7c0cfabaa05
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-14 23:47:52 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  new_arr = []
+  string.split(' ').each do |word|
+    new_arr << word.reverse
+  end
+  new_arr.join(' ')
+  
+  string.split(' ').collect do |word|
+    word.reverse
+  end.join(' ')
+  
+end
