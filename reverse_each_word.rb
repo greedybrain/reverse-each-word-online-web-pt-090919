776481f70d5a64128902700a04424221eca16075
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-14 23:45:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,9 @@ def reverse_each_word(string)
   end
   new_arr.join(' ')
   
-  string.split(' ').collect do |word|
+  string = string.split(' ').collect do |word|
     word.reverse
   end
+  
+  string.join(' ')
 end
